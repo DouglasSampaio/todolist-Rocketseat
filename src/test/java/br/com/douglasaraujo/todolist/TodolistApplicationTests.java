73ec4e085bd88214216c848dcb68ede492b00fee
Douglas Araujo <douglasaraujosampaio@gmail.com>
2023-10-09 19:42:51 -0300

    first commit

---
+++ src/test/java/br/com/douglasaraujo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.douglasaraujo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
